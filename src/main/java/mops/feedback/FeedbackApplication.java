8a4585fbd2b1893992248a03504c92d4ecb7a58f
JanRossbach <janrossbach3@gmail.com>
2020-03-03 09:24:47 +0100

    Initialize Project

---
+++ src/main/java/mops/feedback/FeedbackApplication.java
@@ -0,0 +1,13 @@
+package mops.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeedbackApplication.class, args);
+  }
+
+}
