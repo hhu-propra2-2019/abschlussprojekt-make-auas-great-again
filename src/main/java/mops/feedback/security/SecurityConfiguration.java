654e95325a9fdbca7112af1453a6305e2c7f2367
alalh100 <alalh100@hhu.de>
2020-03-03 11:51:49 +0100

    added formLogin() and logout() Configuration

--- src/main/java/mops/feedback/security/SecurityConfiguration.java
+++ src/main/java/mops/feedback/security/SecurityConfiguration.java
@@ -12,5 +12,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.authorizeRequests();
+        http.formLogin().permitAll();
+        http.logout().permitAll();
     }
 }
