5e731db0bbd35507e2ebe5520cddcf7822665812
alalh100 <alalh100@hhu.de>
2020-03-03 11:50:04 +0100

    Class WebSecurity renamed to SecurityConfiguration

--- src/main/java/mops/feedback/security/SecurityConfiguration.java
+++ src/main/java/mops/feedback/security/SecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
