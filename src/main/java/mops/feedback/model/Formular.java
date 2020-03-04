8cd662c65aff1e1b120dda1c7240e138bde2f6dd
alalh100 <alalh100@hhu.de>
2020-03-04 12:11:49 +0100

    checkstyle problems fixed

--- src/main/java/mops/feedback/model/Formular.java
+++ src/main/java/mops/feedback/model/Formular.java
@@ -1,6 +1,5 @@
 package mops.feedback.model;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Value;
@@ -8,12 +7,17 @@ import lombok.Value;
 @Value
 public class Formular {
 
-  private String Veranstaltungsname;
-  private String Professorenname;
+  private String veranstaltungsname;
+  private String professorenname;
   private List<Frage> fragen;
   private LocalDateTime startdatum;
   private LocalDateTime enddatum;
 
+  /**
+   * gibt uns den Status von dem Formular.
+   * @param  heute als LocalDateTime
+   * @return enum Status ob VERFUEGBAR, NICHTVERFUEGBAR oder ABGESCHLOSSEN
+   */
   public Status getStatus(LocalDateTime heute) {
 
     if (heute.isAfter(startdatum.minusDays(1)) && heute.isBefore(enddatum.plusDays(1))) {
