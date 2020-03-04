d04ebf0f6e57089bc77be4b43814b343397699ee
cms04 <Christian.Morbach@hhu.de>
2020-03-04 21:35:04 +0100

    Add ArchUnit-test for Data-Repositories

---
+++ src/test/java/mops/feedback/archunit/DatabaseArchUnitTest.java
@@ -0,0 +1,16 @@
+package mops.feedback.archunit;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import mops.FeedbackApplication;
+import org.springframework.stereotype.Repository;
+
+@AnalyzeClasses(packagesOf = FeedbackApplication.class)
+public class DatabaseArchUnitTest {
+  @ArchTest
+  static final ArchRule repositoriesareindatabasepackage = classes().that()
+      .areAnnotatedWith(Repository.class).should().haveSimpleNameEndingWith("Repository");
+}
