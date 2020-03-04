6361b9870d5dfe61402f93183797f7427754857e
cms04 <Christian.Morbach@hhu.de>
2020-03-04 21:23:21 +0100

    Move FeedbackApplication-class to package mops

--- src/test/java/mops/feedback/archunit/ControllerArchUnitTest.java
+++ src/test/java/mops/feedback/archunit/ControllerArchUnitTest.java
@@ -5,7 +5,7 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import mops.feedback.FeedbackApplication;
+import mops.FeedbackApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
