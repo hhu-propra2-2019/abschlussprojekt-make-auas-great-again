9a22092678039c54f223610811a45d00d9d6e956
cms04 <Christian.Morbach@hhu.de>
2020-03-05 08:30:26 +0100

    Remove Repository-annotation from Spring-Data-Repository

--- src/main/java/mops/feedback/database/FormularRepository.java
+++ src/main/java/mops/feedback/database/FormularRepository.java
@@ -2,9 +2,7 @@ package mops.feedback.database;
 
 import mops.feedback.database.dto.FormularDto;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FormularRepository extends CrudRepository<FormularDto, Long> {
 
 }
--- src/main/java/mops/feedback/database/FragenRepository.java
+++ src/main/java/mops/feedback/database/FragenRepository.java
@@ -2,9 +2,7 @@ package mops.feedback.database;
 
 import mops.feedback.database.dto.FragenDto;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FragenRepository extends CrudRepository<FragenDto, Long> {
 
 }
