c4ee66667fb16323ee210ea3b4f615797b817588
JanRossbach <janrossbach3@gmail.com>
2020-03-05 20:32:15 +0100

    Add PMD and Supress some unecessary Warnings

--- infrastructure/src/main/java/mops/FeedbackApplication.java
+++ infrastructure/src/main/java/mops/FeedbackApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("PMD.UseUtilityClass")
 public class FeedbackApplication {
 
   public static void main(String[] args) {
--- infrastructure/src/main/java/mops/security/SecurityConfiguration.java
+++ infrastructure/src/main/java/mops/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
+  @SuppressWarnings("PMD.LawOfDemeter")
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
     http.authorizeRequests();
