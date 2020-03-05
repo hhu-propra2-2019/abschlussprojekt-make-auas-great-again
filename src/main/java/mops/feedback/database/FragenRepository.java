41c37e8824e67dd028253a62a879e39cb114f7f7
cms04 <Christian.Morbach@hhu.de>
2020-03-05 08:35:05 +0100

    Remove FragenRepository

--- src/main/java/mops/feedback/database/FragenRepository.java
+++
@@ -1,8 +0,0 @@
-package mops.feedback.database;
-
-import mops.feedback.database.dto.FragenDto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FragenRepository extends CrudRepository<FragenDto, Long> {
-
-}
