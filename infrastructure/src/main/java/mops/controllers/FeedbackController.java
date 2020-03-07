402016d06d8fa2684af2e1cb5908fe491feb6fd8
JanRossbach <janrossbach3@gmail.com>
2020-03-07 12:26:56 +0100

    Edit Controller

--- infrastructure/src/main/java/mops/controllers/FeedbackController.java
+++ infrastructure/src/main/java/mops/controllers/FeedbackController.java
@@ -1,22 +1,30 @@
 package mops.controllers;
 
+import mops.database.FragebogenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("")
 @Controller
 public class FeedbackController {
 
+  @Autowired
+  private FragebogenRepository frageboegen;
+
   @GetMapping("/")
   public String uebersicht(Model model) {
+    model.addAttribute("frageboegen", frageboegen.getAll());
     return "index";
   }
 
   @GetMapping("/details")
-  public String fragebogen(Model model) {
+  public String fragebogen(Model model, @RequestParam Long id) {
+    model.addAttribute("fragebogen", frageboegen.getFragebogenById(id));
     return "details";
   }
 
