d65c52ca349e75ba8fd61b550cb0b4d826274118
JanRossbach <janrossbach3@gmail.com>
2020-03-07 18:29:13 +0100

    Implement Clock (without Logic)

--- model/src/main/java/mops/Fragebogen.java
+++ model/src/main/java/mops/Fragebogen.java
@@ -4,20 +4,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @Builder
-@Value
 @EqualsAndHashCode(of = "bogennr")
+@Getter
+@Setter
 public class Fragebogen {
 
-  Long bogennr;
-  String veranstaltungsname;
-  String professorenname;
-  List<Frage> fragen;
-  LocalDateTime startdatum;
-  LocalDateTime enddatum;
+  private final Long bogennr;
+  private String veranstaltungsname;
+  private String professorenname;
+  private List<Frage> fragen;
+  private LocalDateTime startdatum;
+  private LocalDateTime enddatum;
 
   /**
    * Gibt den Formularstatus zurück.
@@ -26,11 +28,39 @@ public class Fragebogen {
    * @return enum mops.Status ob VERFUEGBAR, NICHTVERFUEGBAR oder ABGESCHLOSSEN
    */
   public Status getStatus(LocalDateTime heute) {
-
     if (heute.isAfter(startdatum.minusDays(1)) && heute.isBefore(enddatum.plusDays(1))) {
       return Status.VERFUEGBAR;
     }
     return Status.NICHTVERFUEGBAR;
+  }
+
+  /**
+   * Gibt die verbleibenden Studen bis zum auslaufen des Fragebogens zurück.
+   *
+   * @return String
+   */
+  public String getRemainingHours() {
+    return "24";
+  }
+
+  /**
+   * Gibt die verbleibenden Minuten bis zum auslaufen des Fragebogens zurück.
+   * Rückgabe ist immer zwischen 0 und 59
+   *
+   * @return String
+   */
+  public String getRemainingMinutes() {
+    return "59";
+  }
+
+  /**
+   * Gibt die verbleibenden Studen bis zum auslaufen des Fragebogens zurück.
+   * Rückgabe ist immer zwischen 0 und 59
+   *
+   * @return String
+   */
+  public String getRemainingSeconds() {
+    return "59";
   }
 }
 
