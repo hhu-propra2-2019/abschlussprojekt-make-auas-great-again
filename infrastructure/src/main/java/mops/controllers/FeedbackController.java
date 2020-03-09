fd4a930fb3f97d288185602acee20b3b640315a4
JanRossbach <janrossbach3@gmail.com>
2020-03-09 10:58:17 +0100

    Fix the /feedback problem

--- infrastructure/src/main/java/mops/controllers/FeedbackController.java
+++ infrastructure/src/main/java/mops/controllers/FeedbackController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping("")
+@RequestMapping("/feedback")
 @Controller
 public class FeedbackController {
 
