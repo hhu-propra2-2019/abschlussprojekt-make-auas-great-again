35bad80b04fa1b4d121fb8d50fcf56188d00cd40
cms04 <Christian.Morbach@hhu.de>
2020-03-10 17:10:30 +0100

    Add test for POST-Request on SubmitController

--- infrastructure/src/test/java/mops/controllers/SubmitControllerTest.java
+++ infrastructure/src/test/java/mops/controllers/SubmitControllerTest.java
@@ -1,6 +1,11 @@
 package mops.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -10,4 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 public class SubmitControllerTest {
   @Autowired
   private transient MockMvc mvc;
+
+  @Test
+  public void submit() throws Exception {
+    mvc.perform(post("/feedback/details/submit/1")).andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/feedback/"));
+  }
 }
