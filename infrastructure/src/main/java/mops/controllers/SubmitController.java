76bbdd4e439ada0420b9508202d61c9aab982f6d
cms04 <Christian.Morbach@hhu.de>
2020-03-10 14:50:32 +0100

    Refactoring in Submit-Controller and -Service

--- infrastructure/src/main/java/mops/controllers/SubmitController.java
+++ infrastructure/src/main/java/mops/controllers/SubmitController.java
@@ -5,24 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import mops.Fragebogen;
+import lombok.AllArgsConstructor;
 import mops.services.SubmitService;
 
 @RequestMapping("/feedback/details/submit")
+@AllArgsConstructor
 @Controller
 public class SubmitController {
-  private final transient FragebogenRepository frageboegen;
   private final transient SubmitService service;
-
-  public SubmitController(FragebogenRepository frageboegen, SubmitService service) {
-    this.frageboegen = frageboegen;
-    this.service = service;
-  }
 
   @PostMapping("/{bogennr}")
   public String submitFeedback(@PathVariable long bogennr, HttpServletRequest req) {
-    Fragebogen fragebogen = frageboegen.getFragebogenById(bogennr);
-    service.saveAntworten(req, fragebogen);
+    service.saveAntworten(req, bogennr);
     return "redirect:/feedback/";
   }
 }
--- infrastructure/src/main/java/mops/services/SubmitService.java
+++ infrastructure/src/main/java/mops/services/SubmitService.java
@@ -3,6 +3,7 @@ package mops.services;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
 import mops.Auswahl;
 import mops.Frage;
 import mops.Fragebogen;
@@ -11,27 +12,39 @@ import mops.MultipleChoiceFrage;
 import mops.TextAntwort;
 import mops.TextFrage;
 import mops.TypeChecker;
+import mops.controllers.FragebogenRepository;
 
 @Service
+@AllArgsConstructor
 public class SubmitService {
+  private final transient FragebogenRepository frageboegen;
 
-  public void saveAntworten(HttpServletRequest req, Fragebogen fragebogen) {
+  public void saveAntworten(HttpServletRequest req, long bogennr) {
+    Fragebogen fragebogen = frageboegen.getFragebogenById(bogennr);
     List<Frage> fragen = fragebogen.getFragen();
     for (Frage frage : fragen) {
       if (TypeChecker.isMultipleChoice(frage)) {
-        List<Auswahl> moeglicheantworten = ((MultipleChoiceFrage) frage).getChoices();
-        Auswahl auswahl = new Auswahl(req.getParameter("answer-" + frage.getId()));
-        if (moeglicheantworten.contains(auswahl)) {
-          ((MultipleChoiceFrage) frage).addAntwort(new MultipleChoiceAntwort(auswahl));
-          System.out.println("Antwort " + auswahl.toString() + " wurde zur Frage '"
-              + frage.getFragentext() + "' hinzugefügt"); // Debugging-Ausgabe, kann später gelöscht
-                                                          // werden
-        }
+        saveMultipleChoiceAntwort(req, frage);
       } else if (TypeChecker.isTextFrage(frage)) {
-        String antwort = req.getParameter("answer-" + frage.getId());
-        ((TextFrage) frage).addAntwort(new TextAntwort(antwort));
-        System.out.println("Antwort auf Frage '" + frage.getFragentext() + "': " + antwort); // Debugging-Ausgabe
+        saveTextantwort(req, frage);
       }
     }
+  }
+
+  private void saveMultipleChoiceAntwort(HttpServletRequest req, Frage frage) {
+    List<Auswahl> moeglicheantworten = ((MultipleChoiceFrage) frage).getChoices();
+    Auswahl auswahl = new Auswahl(req.getParameter("answer-" + frage.getId()));
+    if (moeglicheantworten.contains(auswahl)) {
+      ((MultipleChoiceFrage) frage).addAntwort(new MultipleChoiceAntwort(auswahl));
+      System.out.println("Antwort " + auswahl.toString() + " wurde zur Frage '"
+          + frage.getFragentext() + "' hinzugefügt"); // Debugging-Ausgabe, kann später gelöscht
+                                                      // werden
+    }
+  }
+
+  private void saveTextantwort(HttpServletRequest req, Frage frage) {
+    String antwort = req.getParameter("answer-" + frage.getId());
+    ((TextFrage) frage).addAntwort(new TextAntwort(antwort));
+    System.out.println("Antwort auf Frage '" + frage.getFragentext() + "': " + antwort); // Debugging-Ausgabe
   }
 }
