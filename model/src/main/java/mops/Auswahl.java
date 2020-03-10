252a8e5f797f1e91dc554cd7789186f641e2c122
cms04 <Christian.Morbach@hhu.de>
2020-03-10 16:42:22 +0100

    Remove ToString Annotation from Auswahl class

--- model/src/main/java/mops/Auswahl.java
+++ model/src/main/java/mops/Auswahl.java
@@ -8,7 +8,6 @@ import lombok.ToString;
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode(of = "label")
-@ToString // nur nötig für Debugging-Ausgabe im FeedbackController, kann später gelöscht werden
 public class Auswahl {
   private String label;
 }
