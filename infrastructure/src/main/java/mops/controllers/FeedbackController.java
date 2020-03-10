f39b4ae38ff779f674da3ac8351c6cf1ab401854
melina-richard <melina.richard@outlook.de>
2020-03-10 12:18:28 +0100

    rework contact form logic

--- infrastructure/src/main/java/mops/controllers/FeedbackController.java
+++ infrastructure/src/main/java/mops/controllers/FeedbackController.java
@@ -1,10 +1,15 @@
 package mops.controllers;
 
+import java.time.LocalDateTime;
+import mops.Kontaktformular;
+import mops.Student;
 import mops.TypeChecker;
+import mops.database.MockDozentenRepository;
 import mops.database.MockFragebogenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,11 +21,13 @@ public class FeedbackController {
   private static final String emptySearchString = "";
   private final transient FragebogenRepository frageboegen;
   private final transient TypeChecker typeChecker;
+  private transient DozentenRepository dozenten;
 
 
   public FeedbackController() {
     this.frageboegen = new MockFragebogenRepository();
     this.typeChecker = new TypeChecker();
+    this.dozenten = new MockDozentenRepository();
   }
 
   @GetMapping("/")
@@ -44,13 +51,15 @@ public class FeedbackController {
 
   @GetMapping("/kontakt")
   public String kontakt(Model model) {
+    model.addAttribute("dozenten", dozenten.getAll());
+    model.addAttribute("kontaktformular", new Kontaktformular());
     return "kontakt";
   }
 
   @PostMapping("/kontakt")
-  public String postMessage(Model model) {
-    model.addAttribute("post", "post");
-    model.addAttribute("submit", "submit");
-    return "kontakt";
+  public String postMessage(@ModelAttribute Kontaktformular kontakt, Model model) {
+    kontakt.setStudent(new Student(678L, "a", "b", "a@b.de", 2278));
+    kontakt.setZeitpunkt(LocalDateTime.now());
+    return "index";
   }
 }
