f313eae2f95187a42f1fd6b11d497d83d46272be
cms04 <Christian.Morbach@hhu.de>
2020-03-10 16:09:45 +0100

    Refactoring to respect architecture

--- infrastructure/src/main/java/mops/controllers/SubmitController.java
+++ infrastructure/src/main/java/mops/controllers/SubmitController.java
@@ -1,22 +1,36 @@
 package mops.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import lombok.AllArgsConstructor;
-import mops.services.SubmitService;
+import mops.Frage;
+import mops.Fragebogen;
+import mops.SubmitService;
+import mops.database.MockFragebogenRepository;
 
 @RequestMapping("/feedback/details/submit")
-@AllArgsConstructor
 @Controller
 public class SubmitController {
+  private final transient FragebogenRepository frageboegen;
   private final transient SubmitService service;
+
+  public SubmitController() {
+    frageboegen = new MockFragebogenRepository();
+    service = new SubmitService();
+  }
 
   @PostMapping("/{bogennr}")
   public String submitFeedback(@PathVariable long bogennr, HttpServletRequest req) {
-    service.saveAntworten(req, bogennr);
+    Fragebogen fragebogen = frageboegen.getFragebogenById(bogennr);
+    Map<Long, String> antworten = new HashMap<>();
+    for (Frage frage : fragebogen.getFragen()) {
+      antworten.put(frage.getId(), req.getParameter("answer-" + frage.getId()));
+    }
+    service.saveAntworten(fragebogen, antworten);
     return "redirect:/feedback/";
   }
 }
