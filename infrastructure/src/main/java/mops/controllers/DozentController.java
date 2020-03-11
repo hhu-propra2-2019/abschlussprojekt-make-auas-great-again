1fedc3da5586c8a517370fa8e5fa6f0b1c583f39
cms04 <Christian.Morbach@hhu.de>
2020-03-11 10:02:38 +0100

    Add Index-Page for organization

--- infrastructure/src/main/java/mops/controllers/DozentController.java
+++ infrastructure/src/main/java/mops/controllers/DozentController.java
@@ -1,16 +1,31 @@
 package mops.controllers;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import mops.Fragebogen;
+import mops.TypeChecker;
 import mops.database.MockFragebogenRepository;
 
 @Controller
 @RequestMapping("/feedback/dozenten")
 public class DozentController {
   private final transient FragebogenRepository frageboegen;
+  private final transient TypeChecker typechecker;
 
   public DozentController() {
     frageboegen = new MockFragebogenRepository();
+    typechecker = new TypeChecker();
+  }
+
+  @GetMapping("/")
+  public String getOrganisatorMainPage(Model model) {
+    List<Fragebogen> fragebogenliste = frageboegen.getAll();
+    model.addAttribute("frageboegen", fragebogenliste);
+    model.addAttribute("typechecker", typechecker);
+    return "dozent";
   }
 
 }
