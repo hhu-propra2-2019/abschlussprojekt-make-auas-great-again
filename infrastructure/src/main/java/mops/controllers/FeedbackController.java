3a33fde9b8ed50dc7a98f04884f123bb434210c8
melina-richard <melina.richard@outlook.de>
2020-03-11 10:00:16 +0100

    add model for contact form

--- infrastructure/src/main/java/mops/controllers/FeedbackController.java
+++ infrastructure/src/main/java/mops/controllers/FeedbackController.java
@@ -1,16 +1,10 @@
 package mops.controllers;
 
-import java.time.LocalDateTime;
-import mops.Kontaktformular;
-import mops.Student;
 import mops.TypeChecker;
-import mops.database.MockDozentenRepository;
 import mops.database.MockFragebogenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,13 +15,10 @@ public class FeedbackController {
   private static final String emptySearchString = "";
   private final transient FragebogenRepository frageboegen;
   private final transient TypeChecker typeChecker;
-  private transient DozentenRepository dozenten;
-
 
   public FeedbackController() {
     this.frageboegen = new MockFragebogenRepository();
     this.typeChecker = new TypeChecker();
-    this.dozenten = new MockDozentenRepository();
   }
 
   @GetMapping("/")
@@ -47,19 +38,5 @@ public class FeedbackController {
     model.addAttribute("fragebogen", frageboegen.getFragebogenById(id));
     model.addAttribute("typeChecker", typeChecker);
     return "details";
-  }
-
-  @GetMapping("/kontakt")
-  public String kontakt(Model model) {
-    model.addAttribute("dozenten", dozenten.getAll());
-    model.addAttribute("kontaktformular", new Kontaktformular());
-    return "kontakt";
-  }
-
-  @PostMapping("/kontakt")
-  public String postMessage(@ModelAttribute Kontaktformular kontakt, Model model) {
-    kontakt.setStudent(new Student(678L, "a", "b", "a@b.de", 2278));
-    kontakt.setZeitpunkt(LocalDateTime.now());
-    return "index";
   }
 }
---
+++ model/src/main/java/mops/Student.java
@@ -0,0 +1,13 @@
+package mops;
+
+import lombok.Value;
+
+@Value
+public class Student {
+
+  private Long id;
+  private String vorname;
+  private String nachname;
+  private String email;
+  private Integer matnr;
+}
