8bf956c08a78af36e973489af448aaa7f6e346aa
melina-richard <melina.richard@outlook.de>
2020-03-11 09:50:18 +0100

    improve design of ergebnis.html

--- infrastructure/src/main/java/mops/controllers/ErgebnisController.java
+++ infrastructure/src/main/java/mops/controllers/ErgebnisController.java
@@ -1,6 +1,9 @@
 package mops.controllers;
 
+import mops.TypeChecker;
+import mops.database.MockFragebogenRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class ErgebnisController {
 
+  private final transient FragebogenRepository frageboegen;
+  private final transient TypeChecker typeChecker;
+
+  public ErgebnisController() {
+    this.frageboegen = new MockFragebogenRepository();
+    this.typeChecker = new TypeChecker();
+  }
+
   @GetMapping("/ergebnis")
-  public String boegenUebersicht() {
+  public String boegenUebersicht(Model model) {
+    model.addAttribute("frageboegen", frageboegen.getAll());
+    model.addAttribute("typeChecker", typeChecker);
     return "ergebnis";
   }
 
