b9e01aaf6fa6a4d0046c37ad235aa90a3ad630d1
JanRossbach <janrossbach3@gmail.com>
2020-03-12 21:59:48 +0100

    Fix StudentController Test

--- infrastructure/src/test/java/mops/controllers/StudentControllerTest.java
+++ infrastructure/src/test/java/mops/controllers/StudentControllerTest.java
@@ -1,8 +1,8 @@
 package mops.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 import com.c4_soft.springaddons.test.security.context.support.WithIDToken;
@@ -16,20 +16,31 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
 class StudentControllerTest {
   @Autowired
   private transient MockMvc mvc;
 
   @Test
-  @DisplayName("Studenten sollten auf die Uebersicht kommen")
+  @DisplayName("Student sollte auf die student_uebersicht Seite weitergeleitet werden")
   @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
-  public void studentUebersicht() throws Exception {
-    mvc.perform(post("/feedback/student/")).andExpect(status().is2xxSuccessful());
+  public void correctRedirectFromIndex() throws Exception {
+    mvc.perform(get("/feedback/")).andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/feedback/student/"));
   }
 
   @Test
-  @DisplayName("Test to show that GET is not allowed to avoid side effects")
-  public void getIsNotAllowed() throws Exception {
-    mvc.perform(get("/feedback/student/details/submit/1")).andExpect(status().isMethodNotAllowed());
+  @DisplayName("Studenten kommen auf die Uebersicht Seite")
+  @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
+  public void uebersichtSucess() throws Exception {
+    mvc.perform(get("/feedback/student/")).andExpect(status().is2xxSuccessful())
+        .andExpect(view().name("studenten/student_uebersicht"));
+  }
+
+  @Test
+  @DisplayName("Orga sollte nicht auf die student Uebersicht Seite kommen")
+  @WithMockKeycloackAuth(roles = "orga", idToken = @WithIDToken(email = "user@mail.de"))
+  public void uebersichtFail() throws Exception {
+    mvc.perform(get("/feedback/student/")).andExpect(status().is4xxClientError());
   }
 }
