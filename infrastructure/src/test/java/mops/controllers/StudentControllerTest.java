4b909e87aef58cf47b59d6cabe3125ea9fc7ca04
Chrizz-Chrozz <chrizzchrozz.16@gmail.com>
2020-03-13 11:24:19 +0100

    Added more controller tests for StudentController

--- infrastructure/src/test/java/mops/controllers/StudentControllerTest.java
+++ infrastructure/src/test/java/mops/controllers/StudentControllerTest.java
@@ -1,12 +1,14 @@
 package mops.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 import com.c4_soft.springaddons.test.security.context.support.WithIDToken;
 import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +27,8 @@ class StudentControllerTest {
   @DisplayName("Student sollte auf die student_uebersicht Seite weitergeleitet werden")
   @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
   public void correctRedirectFromIndex() throws Exception {
-    mvc.perform(get("/feedback/")).andExpect(status().is3xxRedirection())
+    mvc.perform(get("/feedback/"))
+        .andExpect(status().is3xxRedirection())
         .andExpect(view().name("redirect:/feedback/student/"));
   }
 
@@ -33,7 +36,8 @@ class StudentControllerTest {
   @DisplayName("Studenten kommen auf die Uebersicht Seite")
   @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
   public void uebersichtSucess() throws Exception {
-    mvc.perform(get("/feedback/student/")).andExpect(status().is2xxSuccessful())
+    mvc.perform(get("/feedback/student/"))
+        .andExpect(status().is2xxSuccessful())
         .andExpect(view().name("studenten/student_uebersicht"));
   }
 
@@ -42,5 +46,41 @@ class StudentControllerTest {
   @WithMockKeycloackAuth(roles = "orga", idToken = @WithIDToken(email = "user@mail.de"))
   public void uebersichtFail() throws Exception {
     mvc.perform(get("/feedback/student/")).andExpect(status().is4xxClientError());
+  }
+
+  @Test
+  @DisplayName("Student sollte auf die 'student_details'-Seite weitergeleitet werden.")
+  @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
+  public void correctRedirectForDetails() throws Exception {
+    mvc.perform(get("/feedback/student/details").param("id", "1"))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(view().name("studenten/student_details"));
+  }
+
+  @Test
+  @DisplayName("Student sollte auf die 'kontakt'-Seite weitergeleitet werden.")
+  @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
+  public void correctRedirectForKontakt() throws Exception {
+    mvc.perform(get("/feedback/student/kontakt"))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(view().name("/studenten/kontakt"));
+  }
+
+  @Test
+  @DisplayName("Student sollte auf die 'ergebnis'-Seite weitergeleitet werden.")
+  @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
+  public void correctRedirectForErgebnis() throws Exception {
+    mvc.perform(get("/feedback/student/ergebnis"))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(view().name("/studenten/ergebnis"));
+  }
+
+  @Test
+  @DisplayName("Student sollte auf die 'ergebnisUebersicht'-Seite weitergeleitet werden.")
+  @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = "user@mail.de"))
+  public void correctRedirectForErgebnisUebersicht() throws Exception {
+    mvc.perform(get("/feedback/student/ergebnisUebersicht").param("id", "1"))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(view().name("/studenten/ergebnisUebersicht"));
   }
 }
