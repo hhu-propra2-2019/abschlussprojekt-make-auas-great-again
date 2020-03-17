70383371f194e31e1bf88c6fd9853e257f7555c2
cms04 <Christian.Morbach@hhu.de>
2020-03-17 09:40:13 +0100

    Add functions to find text answer by id

--- model/src/main/java/mops/antworten/TextAntwort.java
+++ model/src/main/java/mops/antworten/TextAntwort.java
@@ -1,10 +1,12 @@
 package mops.antworten;
 
+import java.util.Random;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value
 @AllArgsConstructor
 public class TextAntwort extends Antwort {
+  private final Long id = Long.valueOf(new Random().nextInt(1000));
   private String antworttext;
 }
