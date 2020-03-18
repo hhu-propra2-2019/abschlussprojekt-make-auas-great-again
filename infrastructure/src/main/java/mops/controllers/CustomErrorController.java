4c380880bcb5b45f41e0a1bf8b7fb29c08f4805f
RogerSellin <Roger.Sellin@uni-duesseldorf.de>
2020-03-18 13:16:20 +0100

    refactorin everything is now checkstyle compliant

--- infrastructure/src/main/java/mops/controllers/CustomErrorController.java
+++ infrastructure/src/main/java/mops/controllers/CustomErrorController.java
@@ -10,14 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
   @RequestMapping("/error")
-  public String handleError(HttpServletRequest request){
+  public String handleError(HttpServletRequest request) {
     Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
     Integer statusCode = Integer.valueOf(status.toString());
-    if(statusCode == HttpStatus.SC_FORBIDDEN)
-    return "403";
-
+    if (statusCode == HttpStatus.SC_FORBIDDEN) {
+      return "403";
+    }
     return "error";
   }
+
   @Override
   public String getErrorPath() {
     return "/error";
