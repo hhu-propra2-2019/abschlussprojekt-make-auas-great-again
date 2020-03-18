fcc02dc1a548d0c0cbdc28b6ff565110ce114a01
RogerSellin <Roger.Sellin@uni-duesseldorf.de>
2020-03-18 13:08:10 +0100

    add CustomErrorController

---
+++ infrastructure/src/main/java/mops/controllers/CustomErrorController.java
@@ -0,0 +1,25 @@
+package mops.controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.http.HttpStatus;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+  @RequestMapping("/error")
+  public String handleError(HttpServletRequest request){
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    Integer statusCode = Integer.valueOf(status.toString());
+    if(statusCode == HttpStatus.SC_FORBIDDEN)
+    return "403";
+
+    return "error";
+  }
+  @Override
+  public String getErrorPath() {
+    return "/error";
+  }
+}
