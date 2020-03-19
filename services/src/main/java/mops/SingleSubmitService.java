c395c566b20d0b3ede5e5867582098e64d4c38c0
alalh100 <alalh100@hhu.de>
2020-03-19 09:22:27 +0100

    added hasSubmitted method to easily check if a student has submitted or not
    Signed-off-by: alalh100 <alalh100@hhu.de>

--- services/src/main/java/mops/SingleSubmitService.java
+++ services/src/main/java/mops/SingleSubmitService.java
@@ -1,10 +1,21 @@
 package mops;
 
+import java.util.List;
 import mops.rollen.Student;
 
 public class SingleSubmitService {
 
   public void addStudentAsSubmitted(Fragebogen fragebogen, Student student) {
     fragebogen.addStudentAsSubmitted(student);
+  }
+
+  public boolean hasSubmitted(Fragebogen fragebogen, Student student) {
+    List<Student> abgegebeneStudierende = fragebogen.getAbgegebeneStudierende();
+    for (Student studentin : abgegebeneStudierende) {
+      if (studentin.equals(student)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
