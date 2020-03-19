0cde92d2dabcbc5e3234705b596fdeed9113355a
cms04 <Christian.Morbach@hhu.de>
2020-03-19 10:06:43 +0100

    Make save method in VeranstaltungsRepository public

--- infrastructure/src/main/java/mops/database/MockVeranstaltungsRepository.java
+++ infrastructure/src/main/java/mops/database/MockVeranstaltungsRepository.java
@@ -26,7 +26,8 @@ public class MockVeranstaltungsRepository implements VeranstaltungsRepository {
     }
   }
 
-  private void save(Veranstaltung veranstaltung) {
+  @Override
+  public void save(Veranstaltung veranstaltung) {
     veranstaltungen.put(veranstaltung.getVeranstaltungsNr(), veranstaltung);
   }
 
@@ -83,12 +84,10 @@ public class MockVeranstaltungsRepository implements VeranstaltungsRepository {
     getAllFromDozent(dozent).stream().forEach(x -> frageboegen.addAll(x.getFrageboegen()));
     return frageboegen.stream().filter(x -> x.getBogennr().equals(id)).findFirst().get();
   }
-  
+
   @Override
   public Fragebogen getFragebogenByIdFromVeranstaltung(Long fragebogen, Long veranstaltung) {
     return veranstaltungen.get(veranstaltung).getFrageboegen().stream()
-        .filter(bogen -> bogen.getBogennr().equals(fragebogen))
-        .findFirst()
-        .get();
+        .filter(bogen -> bogen.getBogennr().equals(fragebogen)).findFirst().get();
   }
 }
