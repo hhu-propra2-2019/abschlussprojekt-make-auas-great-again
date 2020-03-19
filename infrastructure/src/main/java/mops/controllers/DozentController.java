9aa3b1449063056f7fee03fad33d8b407879435d
cms04 <Christian.Morbach@hhu.de>
2020-03-19 09:32:57 +0100

    Show lecturer's events on main page

--- infrastructure/src/main/java/mops/controllers/DozentController.java
+++ infrastructure/src/main/java/mops/controllers/DozentController.java
@@ -1,17 +1,43 @@
 package mops.controllers;
 
 import javax.annotation.security.RolesAllowed;
+import mops.database.MockVeranstaltungsRepository;
+import mops.rollen.Dozent;
+import mops.security.Account;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 @RequestMapping("/feedback/dozenten")
 public class DozentController {
+  private final VeranstaltungsRepository veranstaltungen;
+
+  public DozentController() {
+    veranstaltungen = new MockVeranstaltungsRepository();
+  }
+
   @GetMapping("")
   @RolesAllowed("ROLE_orga")
-  public String getOrganisatorMainPage() {
+  public String getOrganisatorMainPage(KeycloakAuthenticationToken token, Model model) {
+    model.addAttribute("account", createAccountFromPrincipal(token));
+    model.addAttribute("veranstaltungen",
+        veranstaltungen.getAllFromDozent(createDozentFromToken(token)));
     return "dozenten/dozent";
+  }
+
+  private Account createAccountFromPrincipal(KeycloakAuthenticationToken token) {
+    KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    return new Account(principal.getName(),
+        principal.getKeycloakSecurityContext().getIdToken().getEmail(), null,
+        token.getAccount().getRoles());
+  }
+
+  private Dozent createDozentFromToken(KeycloakAuthenticationToken token) {
+    KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    return new Dozent(principal.getName());
   }
 }
