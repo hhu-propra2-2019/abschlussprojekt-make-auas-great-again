6662454ca2fe56cbb464226c27ce987fce765d5d
alalh100 <alalh100@hhu.de>
2020-03-20 09:52:33 +0100

    added test for addStudentAsSubmitted method in SubmitService
    Signed-off-by: alalh100 <alalh100@hhu.de>

--- services/src/test/java/mops/SubmitServiceTest.java
+++ services/src/test/java/mops/SubmitServiceTest.java
@@ -22,12 +22,17 @@ public class SubmitServiceTest {
   private transient SubmitService service = new SubmitService();
   private transient Fragebogen mockFragebogen;
   private transient TextFrage textfrage;
+  private transient Student student;
+  private transient Fragebogen fragebogen;
 
   @BeforeEach
   public void setUp() {
     mockFragebogen = mock(Fragebogen.class);
     textfrage = new TextFrage(Long.valueOf(1), "Beispielfrage");
     when(mockFragebogen.getFragen()).thenReturn(List.of(textfrage));
+    student = new Student("studentin");
+    fragebogen  = new Fragebogen("Programmierung", "Jens", null, null, Einheit.VORLESUNG);
+
   }
 
   @Test
@@ -64,21 +69,26 @@ public class SubmitServiceTest {
   }
 
   @Test
+  @DisplayName("Student als abgegebener in dem entsprechenden Fragebogen speichern ")
+  public void addStudentAsSubmitted(){
+
+    service.addStudentAsSubmitted(fragebogen, student);
+
+    assertTrue(fragebogen.getAbgegebeneStudierende().contains(student));
+  }
+
+  @Test
   @DisplayName("beantwortete Frageboegen Werden für den Student nicht mehr gezeigt")
   public void getNotSubmittedFrageboegen(){
-    Student student = new Student("studentin");
-    Fragebogen fragebogen = mock(Fragebogen.class);
     List<Fragebogen> fragebogenList = new ArrayList<>();
     List<Fragebogen> notSubmittedFrageboegen;
     fragebogenList.add(mockFragebogen);
     fragebogenList.add(fragebogen);
-    when(mockFragebogen.getAbgegebeneStudierende())
-        .thenReturn(new ArrayList<>(Collections.singletonList(student)));
 
-    service.addStudentAsSubmitted(mockFragebogen, student);
+    service.addStudentAsSubmitted(fragebogen , student);
     notSubmittedFrageboegen = service.notSubmittedFrageboegen(fragebogenList, student);
 
-    assertFalse(notSubmittedFrageboegen.contains(mockFragebogen));
+    assertFalse(notSubmittedFrageboegen.contains(fragebogen));
 
   }
 }
