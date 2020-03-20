1db468bff36d7e2467691dda6f435516d80e226d
alalh100 <alalh100@hhu.de>
2020-03-20 09:19:40 +0100

    write a test for notSubmittedFrageboegen method in SubmitService
    Signed-off-by: alalh100 <alalh100@hhu.de>

--- services/src/test/java/mops/SubmitServiceTest.java
+++ services/src/test/java/mops/SubmitServiceTest.java
@@ -4,14 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import mops.antworten.TextAntwort;
 import mops.fragen.TextFrage;
+import mops.rollen.Student;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings( {"PMD.JUnitTestsShouldIncludeAssert", "PMD.JUnitAssertionsShouldIncludeMessage"})
@@ -58,5 +61,24 @@ public class SubmitServiceTest {
 
     Set<TextAntwort> antworten = textfrage.getAntworten();
     assertTrue(antworten.isEmpty());
+  }
+
+  @Test
+  @DisplayName("beantwortete Frageboegen Werden für den Student nicht mehr gezeigt")
+  public void getNotSubmittedFrageboegen(){
+    Student student = new Student("studentin");
+    Fragebogen fragebogen = mock(Fragebogen.class);
+    List<Fragebogen> fragebogenList = new ArrayList<>();
+    List<Fragebogen> notSubmittedFrageboegen;
+    fragebogenList.add(mockFragebogen);
+    fragebogenList.add(fragebogen);
+    when(mockFragebogen.getAbgegebeneStudierende())
+        .thenReturn(new ArrayList<>(Collections.singletonList(student)));
+
+    service.addStudentAsSubmitted(mockFragebogen, student);
+    notSubmittedFrageboegen = service.notSubmittedFrageboegen(fragebogenList, student);
+
+    assertFalse(notSubmittedFrageboegen.contains(mockFragebogen));
+
   }
 }
