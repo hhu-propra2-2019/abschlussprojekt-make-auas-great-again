d4e42ddbd4e9cfed6fdc5052fdd8f5fa7b2c36c5
nauth100 <naseem_lovely_n@hotmail.com>
2020-03-22 20:42:58 +0100

    LogoutControllerTest added, CustomErrorControllerTest tests WIP.

---
+++ infrastructure/src/test/java/mops/controllers/CustomErrorControllerTest.java
@@ -0,0 +1,38 @@
+package mops.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.c4_soft.springaddons.test.security.context.support.WithIDToken;
+import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+public class CustomErrorControllerTest {
+
+  private final transient String userrole = "studentin";
+  private final transient String usermail = "user@mail.de";
+  private final transient String orgarole = "orga";
+  private final transient String orgamail = "orga@mail.de";
+
+  @Autowired
+  private transient MockMvc mvc;
+
+  @Disabled
+  @Test
+  @DisplayName("Student sollte zu Error Page weitergeleitet werden, wenn die Dozenten Page aufgerufen wird")
+  @WithMockKeycloackAuth(roles = "studentin", idToken = @WithIDToken(email = usermail))
+  public void StudentErrorPageDisplay() throws Exception {
+    mvc.perform(get("/dozenten")).andExpect(status().is4xxClientError())
+        .andExpect(redirectedUrl("/error"));
+  }
+}
