e5f667d8f41791bd20791c0c12e294cc447da31b
cms04 <Christian.Morbach@hhu.de>
2020-03-23 14:38:15 +0100

    Add test for getTextAntwort method

--- services/src/test/java/mops/DozentServiceTest.java
+++ services/src/test/java/mops/DozentServiceTest.java
@@ -1,13 +1,14 @@
 package mops;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import mops.antworten.TextAntwort;
 import mops.fragen.Frage;
 import mops.fragen.MultipleResponseFrage;
 import mops.fragen.SingleResponseFrage;
 import mops.fragen.TextFrage;
+import org.junit.jupiter.api.Test;
 
 public class DozentServiceTest {
   private final DozentService service = new DozentService();
@@ -48,9 +49,27 @@ public class DozentServiceTest {
     TextFrage frage = new TextFrage(1L, "Wie geht's?");
     TextFrage frage2 = new TextFrage(2L, "Sind sie zufrieden mit der Veranstaltung?");
     fragebogen.addFrage(frage);
+    fragebogen.addFrage(frage2);
 
     Frage totest = service.getFrage(1L, fragebogen);
 
     assertEquals(totest, frage);
+  }
+
+  @Test
+  public void korrekteTextAntwortWirdZurueckGegeben() {
+    TextAntwort antwort = new TextAntwort(1L, "Sehr gut");
+    TextAntwort antwort2 = new TextAntwort(2L, "Sehr schlecht");
+    TextFrage frage = new TextFrage(1L, "Wie geht's?");
+    frage.addAntwort(antwort);
+    frage.addAntwort(antwort2);
+    TextFrage frage2 = new TextFrage(2L, "Sind Sie zufrieden?");
+    Fragebogen fragebogen = new Fragebogen("Analysis I", "Heinz Mustermann");
+    fragebogen.addFrage(frage2);
+    fragebogen.addFrage(frage);
+
+    TextAntwort totest = service.getTextAntwort(1L, 2L, fragebogen);
+
+    assertEquals(totest, antwort2);
   }
 }
