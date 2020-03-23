8c5a007ed426883a49089d26ca6be557ada97e89
nauth100 <naseem_lovely_n@hotmail.com>
2020-03-23 11:09:21 +0100

    Added Services ArchTests, Packages should be Updated.

--- infrastructure/src/test/java/mops/archunit/ArchLawsTests.java
+++ infrastructure/src/test/java/mops/archunit/ArchLawsTests.java
@@ -1,8 +1,11 @@
 package mops.archunit;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+import static mops.archunit.ArchRulesConfig.MOPS;
 import static mops.archunit.ArchRulesConfig.MOPS_CONTROLLERS;
 import static mops.archunit.ArchRulesConfig.MOPS_CONTROLLERS_BASE;
+import static mops.archunit.ArchRulesConfig.MOPS_SERVICES;
 
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
@@ -11,13 +14,14 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 
 @AnalyzeClasses(importOptions = ImportOption.DoNotIncludeTests.class, packages = "mops")
 public class ArchLawsTests {
+
   //@TODO REMOVE THE REPOSITORY CLASSES FROM THE CONTROLLERS PACKAGE
 
-  @ArchIgnore
   @ArchTest
   static final ArchRule ALL_CONTROLLERS_SHOULD_RESIDE_IN_CONTROLLERS_PACKAGE = classes()
       .that()
@@ -26,6 +30,7 @@ public class ArchLawsTests {
       .resideOutsideOfPackage(MOPS_CONTROLLERS)
       .should()
       .notBeAnnotatedWith(Controller.class);
+
   //@TODO REMOVE THE REPOSITORY CLASSES FROM THE CONTROLLERS PACKAGE
 
   @ArchIgnore
@@ -37,4 +42,23 @@ public class ArchLawsTests {
       .areNotAnnotatedWith(WebMvcTest.class)
       .should()
       .beAnnotatedWith(Controller.class);
+
+  @ArchTest
+  static final ArchRule CYCLES_INSIDE_THE_PACKAGES_TEST = slices()
+      .matching(MOPS + "..")
+      .should()
+      .beFreeOfCycles();
+
+  //@TODO ADD SERVICES PACKAGE AND INCLUDED ALL SERVICES THERE
+
+  @ArchIgnore
+  @ArchTest
+  static final ArchRule SERVICES_SHOULD_ANNOTATED_WITH_SERVICE = classes()
+      .that()
+      .haveSimpleNameContaining("Service")
+      .and()
+      .resideInAPackage(MOPS_SERVICES)
+      .should()
+      .beAnnotatedWith(Service.class);
 }
+
--- infrastructure/src/test/java/mops/archunit/ArchRulesConfig.java
+++ infrastructure/src/test/java/mops/archunit/ArchRulesConfig.java
@@ -5,6 +5,8 @@ class ArchRulesConfig {
   static final String MOPS = "mops";
   static final String MOPS_CONTROLLERS_BASE = MOPS + ".controllers";
   static final String MOPS_CONTROLLERS = MOPS_CONTROLLERS_BASE + "..";
+  static final String MOPS_SERVICES_BASE = MOPS + ".services";
+  static final String MOPS_SERVICES = MOPS + ".services..";
   static final String MOPS_PRESESTINCE = MOPS + ".database..";
 
 }
