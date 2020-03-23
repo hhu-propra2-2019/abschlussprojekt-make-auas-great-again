5d814adf9798cbeeb17430a2883997ba53b441d9
JanRossbach <janrossbach3@gmail.com>
2020-03-23 10:33:01 +0100

    Redisign Veranstaltungs Uebersicht
    Signed-off-by: JanRossbach <janrossbach3@gmail.com>

--- services/src/main/java/mops/VeranstaltungsService.java
+++ services/src/main/java/mops/VeranstaltungsService.java
@@ -13,7 +13,7 @@ public class VeranstaltungsService {
 
   private Veranstaltung randomVeranstaltung() {
     Veranstaltung.VeranstaltungBuilder veranstaltung = Veranstaltung.builder();
-    Dozent dozent = new Dozent("orga1", "jens", "B");
+    Dozent dozent = new Dozent("orga1", "jens", "Bendisposto");
     veranstaltung = veranstaltung.dozent(dozent).name("Programmierung").semester("SOSE2019")
         .studenten(randomStudenten()).frageboegen(frageboegen.randomFrageboegen(10))
         .veranstaltungsNr((long) idGenerator.nextInt(1000));
