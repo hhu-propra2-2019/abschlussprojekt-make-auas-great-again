f72d0e0aca18357c7f8449a67f6c390bc1799d91
cms04 <Christian.Morbach@hhu.de>
2020-03-24 20:13:26 +0100

    Add AllArgsConstructor to questionnaire template

--- model/src/main/java/mops/FragebogenTemplate.java
+++ model/src/main/java/mops/FragebogenTemplate.java
@@ -3,12 +3,14 @@ package mops;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import mops.fragen.Frage;
 import mops.fragen.MultipleChoiceFrage;
 
 @Getter
+@AllArgsConstructor
 @EqualsAndHashCode(of = "name")
 public class FragebogenTemplate {
   private final Long id;
