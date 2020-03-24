7e85225f6f6dbac962b1c29d621223b35111de65
cms04 <Christian.Morbach@hhu.de>
2020-03-24 08:51:37 +0100

    Add template page

---
+++ infrastructure/src/main/java/mops/controllers/DozentTemplateController.java
@@ -0,0 +1,32 @@
+package mops.controllers;
+
+import mops.rollen.Dozent;
+import mops.security.Account;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/feedback/dozenten/templates")
+public class DozentTemplateController {
+  @GetMapping("")
+  public String getTemplatePage(Model model, KeycloakAuthenticationToken token) {
+    model.addAttribute("account", createAccountFromPrincipal(token));
+    return "dozenten/templates";
+  }
+
+  private Account createAccountFromPrincipal(KeycloakAuthenticationToken token) {
+    KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    return new Account(principal.getName(),
+        principal.getKeycloakSecurityContext().getIdToken().getEmail(), null,
+        token.getAccount().getRoles());
+  }
+
+  private Dozent createDozentFromToken(KeycloakAuthenticationToken token) {
+    KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    return new Dozent(principal.getName());
+  }
+}
