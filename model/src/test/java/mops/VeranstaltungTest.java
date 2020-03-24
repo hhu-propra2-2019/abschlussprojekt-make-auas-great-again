599bb56fd40f3f2b5f9e0014f7d9a876ad09d5b3
nauth100 <naseem_lovely_n@hotmail.com>
2020-03-24 11:20:24 +0100

    Added VeranstaltungsTest.

---
+++ model/src/test/java/mops/VeranstaltungTest.java
@@ -0,0 +1,32 @@
+package mops;
+
+import java.util.List;
+import mops.rollen.Dozent;
+import mops.rollen.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Veranstaltung Methods Test")
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+class VeranstaltungTest {
+
+  private transient Veranstaltung veranstaltung;
+
+  @BeforeEach
+  public void setUp() {
+    veranstaltung = new Veranstaltung("ProPra2", "3", new Dozent("Jens"));
+  }
+
+  @Test
+  public void addStudentTest() {
+    Student student = new Student("nauth100");
+    veranstaltung.addStudent(student);
+    List<Student> studenten = veranstaltung.getStudenten();
+    Assertions.assertTrue(studenten.contains(student));
+
+  }
+
+
+}
