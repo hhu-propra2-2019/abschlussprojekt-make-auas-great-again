1524e68efd6b297864cf6d0440c40af0784d1012
JanRossbach <janrossbach3@gmail.com>
2020-03-25 09:40:04 +0100

    Add empty Query

--- infrastructure/src/main/java/mops/database/DozentenJdbcRepository.java
+++ infrastructure/src/main/java/mops/database/DozentenJdbcRepository.java
@@ -1,7 +1,11 @@
 package mops.database;
 
 import mops.database.dto.DozentDto;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DozentenJdbcRepository extends CrudRepository<DozentDto, Long> {
+  // TODO
+  @Query("")
+  Long findId(String username);
 }
