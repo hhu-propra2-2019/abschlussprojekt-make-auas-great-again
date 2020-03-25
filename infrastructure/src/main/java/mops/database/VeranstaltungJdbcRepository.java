362a31bd22cae16f6f3c0f2abc13d4c5ddc7c7a2
melina-richard <melina.richard@outlook.de>
2020-03-25 14:39:50 +0100

    checkstyle fixes

--- infrastructure/src/main/java/mops/database/VeranstaltungJdbcRepository.java
+++ infrastructure/src/main/java/mops/database/VeranstaltungJdbcRepository.java
@@ -11,20 +11,22 @@ public interface VeranstaltungJdbcRepository extends CrudRepository<Veranstaltun
   @Query("select * from veranstaltung where upper(name) like '%'|| upper(:search) || '%'")
   Set<VeranstaltungDto> getAllContaining(@Param("search") String search);
 
-  @Query("select * from veranstaltung where id in(" +
-      "select veranstaltung from studentBelegtVeranstaltung where student= :tofind)")
-  Set<VeranstaltungDto> getAllFromStudent(@Param("tofind") Long tofind);
+  @Query("select * from veranstaltung where id in("
+      + "select veranstaltung from studentBelegtVeranstaltung where student= :student)")
+  Set<VeranstaltungDto> getAllFromStudent(@Param("student") Long student);
 
-  @Query("select * from veranstaltung where veranstaltung.dozent= :tofind")
-  Set<VeranstaltungDto> getAllFromDozent(@Param("tofind") Long tofind);
+  @Query("select * from veranstaltung where veranstaltung.dozent= :dozent")
+  Set<VeranstaltungDto> getAllFromDozent(@Param("dozent") Long dozent);
 
-  @Query("select * from veranstaltung where id in(" +
-      "select veranstaltung from studentBelegtVeranstaltung where student= :tofind)" +
-      "and upper(name) like '%'|| upper(:search) || '%' ")
-  Set<VeranstaltungDto> getAllFromStudentContaining(@Param("tofind") Long tofind, @Param("search") String search);
+  @Query("select * from veranstaltung where id in("
+      + "select veranstaltung from studentBelegtVeranstaltung where student= :student)"
+      + "and upper(name) like '%'|| upper(:search) || '%' ")
+  Set<VeranstaltungDto> getAllFromStudentContaining(@Param("student") Long student,
+                                                    @Param("search") String search);
 
-  @Query("select * from veranstaltung where veranstaltung.dozent = :tofind " +
-      "and upper(name) like '%'|| upper(:search) || '%' ")
-  Set<VeranstaltungDto> getAllFromDozentContaining(@Param("tofind") Long tofind, @Param("search") String search);
+  @Query("select * from veranstaltung where veranstaltung.dozent = :dozent "
+      + "and upper(name) like '%'|| upper(:search) || '%' ")
+  Set<VeranstaltungDto> getAllFromDozentContaining(@Param("dozent") Long dozent,
+                                                   @Param("search") String search);
 
 }
