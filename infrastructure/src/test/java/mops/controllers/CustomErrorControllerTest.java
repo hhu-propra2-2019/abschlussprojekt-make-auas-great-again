5e98622445a400901fa5601088f86cb02fcc4bf1
nauth100 <naseem_lovely_n@hotmail.com>
2020-03-26 09:45:34 +0100

    CustomErrorPage testing WIP

--- infrastructure/src/test/java/mops/controllers/CustomErrorControllerTest.java
+++ infrastructure/src/test/java/mops/controllers/CustomErrorControllerTest.java
@@ -1,12 +1,10 @@
 package mops.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.c4_soft.springaddons.test.security.context.support.WithIDToken;
 import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +23,11 @@ public class CustomErrorControllerTest {
   @Autowired
   private transient MockMvc mvc;
 
-  @Disabled
+
   @Test
   @DisplayName("Weiterleitung zu Errorpage wenn die Dozenten Page von studenten aufgerufen wird")
   @WithMockKeycloackAuth(roles = userrole, idToken = @WithIDToken(email = usermail))
   public void studentErrorPageDisplay() throws Exception {
-    mvc.perform(get("/dozenten")).andExpect(status().is4xxClientError())
-        .andExpect(redirectedUrl("errorpages/.."));
+    mvc.perform(get("/dozenten")).andExpect(status().is4xxClientError());
   }
 }
