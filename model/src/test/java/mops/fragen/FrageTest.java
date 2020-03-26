a31317db8cce01f3c849f9518418d98e0d7ab356
nauth100 <naseem_lovely_n@hotmail.com>
2020-03-26 08:46:52 +0100

    Added FrageTests, CheckStyle Conforming

--- model/src/test/java/mops/fragen/FrageTest.java
+++ model/src/test/java/mops/fragen/FrageTest.java
@@ -3,43 +3,48 @@ package mops.fragen;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
 public class FrageTest {
 
-  /***
-   All the classes MultipleChoiceFrage,MultipleResponseFrage,SingleResponseFrage,TextFrage
-   Should be tested seperately since Frage is an abstract class.
-   ***/
+  /**
+   * All the classes
+   * MultipleChoiceFrage
+   * MultipleResponseFrage
+   * SingleResponseFrage
+   * TextFrage
+   * Should be tested seperately since Frage is an abstract class.
+   */
 
   @Test
   public void aendereOeffentlichkeitsStatusMultipleChoiceFrageTest() {
-    Frage multipleChoiceFrage = new MultipleChoiceFrage("x");
-    boolean before = multipleChoiceFrage.isOeffentlich();
-    multipleChoiceFrage.aendereOeffentlichkeitsStatus();
-    Assertions.assertNotEquals(before, multipleChoiceFrage.isOeffentlich());
+    Frage multiplechoicefrage = new MultipleChoiceFrage("x");
+    boolean before = multiplechoicefrage.isOeffentlich();
+    multiplechoicefrage.aendereOeffentlichkeitsStatus();
+    Assertions.assertNotEquals(before, multiplechoicefrage.isOeffentlich());
   }
 
   @Test
   public void aendereOeffentlichkeitsStatusMultipleResponseFrageTest() {
-    Frage MultipleResponseFrage = new MultipleResponseFrage(0L, "x");
-    boolean before = MultipleResponseFrage.isOeffentlich();
-    MultipleResponseFrage.aendereOeffentlichkeitsStatus();
-    Assertions.assertNotEquals(before, MultipleResponseFrage.isOeffentlich());
+    Frage multipleresponsefrage = new MultipleResponseFrage(0L, "x");
+    boolean before = multipleresponsefrage.isOeffentlich();
+    multipleresponsefrage.aendereOeffentlichkeitsStatus();
+    Assertions.assertNotEquals(before, multipleresponsefrage.isOeffentlich());
   }
 
   @Test
   public void aendereOeffentlichkeitsStatusSingleResponseFrageTest() {
-    Frage SingleResponseFrage = new SingleResponseFrage(1L, "y", false);
-    boolean before = SingleResponseFrage.isOeffentlich();
-    SingleResponseFrage.aendereOeffentlichkeitsStatus();
-    Assertions.assertNotEquals(before, SingleResponseFrage.isOeffentlich());
+    Frage singleresponsefrage = new SingleResponseFrage(1L, "y", false);
+    boolean before = singleresponsefrage.isOeffentlich();
+    singleresponsefrage.aendereOeffentlichkeitsStatus();
+    Assertions.assertNotEquals(before, singleresponsefrage.isOeffentlich());
   }
 
   @Test
   public void aendereOeffentlichkeitsStatusTextFrageTest() {
-    Frage TextFrage = new TextFrage(0L, "x");
-    boolean before = TextFrage.isOeffentlich();
-    TextFrage.aendereOeffentlichkeitsStatus();
-    Assertions.assertNotEquals(before, TextFrage.isOeffentlich());
+    Frage textfrage = new TextFrage(0L, "x");
+    boolean before = textfrage.isOeffentlich();
+    textfrage.aendereOeffentlichkeitsStatus();
+    Assertions.assertNotEquals(before, textfrage.isOeffentlich());
   }
 
 }
